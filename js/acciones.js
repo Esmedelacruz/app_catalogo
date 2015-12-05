95b5eb080729f9a06fea49c5b42fd375397e765d
Esmedelacruz <damcb164e13@gmail.com>
2015-12-05 12:07:05 -0600

    s

--- js/acciones.js
+++ js/acciones.js
@@ -14,6 +14,7 @@ document.addEventListener("deviceready",function(){
 			$('#contenido').empty();
 			for(var $x=0;$x < producto.tenis.length;$x++)
 			{$('#contenido').append('<div><div align="center"><table width="618" height="392" border="5" ><tr><th width="213"height="378" bgcolor="#00FF99"><img src="http://192.168.1.169/proyecto_catalogo/recursos/imagenes/fotos/'+ producto.tenis[x].clave_zapato +'.jpg" width="213" height="343"></th><th width="354" bgcolor="#00FF99"><p>'+ producto.tenis[$x].modelo +' </p><p>'+ producto.tenis[$x].marca +'</p><p>'+ producto.tenis[$x].color_disponible +'</p><p>'+ producto.tenis[$x].precio +'</p><p>'+ producto.tenis[$x].descripcion +'</p><p>'+ producto.tenis[$x].talla_disponible +'</p></th></tr></table></div></div>');}
+				alert ("entraste a la lista");
 			$(':mobile-pagecontainer').pagecontainer('change','#pagina1',{transition:'pop'});
 			
 			}
