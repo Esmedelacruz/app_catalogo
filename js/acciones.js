52bd2ce9cad494e9d3f4af2467b44be8ae81c512
Esmedelacruz <damcb164e13@gmail.com>
2015-12-05 12:29:05 -0600

    subid31

--- js/acciones.js
+++ js/acciones.js
@@ -4,7 +4,7 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	$('#btnlista').on('tap',function(){
 	alert ("entraste a la lista");
-     $.ajax({
+    $.ajax({
 			type: "POST",
 			url: "http://192.168.1.169/proyecto_catalogo/buscartodo.php",
 			data: "",
@@ -13,8 +13,11 @@ document.addEventListener("deviceready",function(){
 				var producto = JSON.parse(respuesta);
 			$('#contenido').empty();
 			for(var $x=0;$x < producto.tenis.length;$x++)
-			{$('#contenido').append('<div><div align="center"><table width="618" height="392" border="5" ><tr><th width="213"height="378" bgcolor="#00FF99"><img src="http://192.168.1.169/proyecto_catalogo/recursos/imagenes/fotos/'+ producto.tenis[x].clave_zapato +'.jpg" width="213" height="343"></th><th width="354" bgcolor="#00FF99"><p>'+ producto.tenis[$x].modelo +' </p><p>'+ producto.tenis[$x].marca +'</p><p>'+ producto.tenis[$x].color_disponible +'</p><p>'+ producto.tenis[$x].precio +'</p><p>'+ producto.tenis[$x].descripcion +'</p><p>'+ producto.tenis[$x].talla_disponible +'</p></th></tr></table></div></div>');}
-				alert ("entraste a la lista 2");
+			{
+								alert ("entraste a la lista 2");
+
+				
+				$('#contenido').append('<div><div align="center"><table width="618" height="392" border="5" ><tr><th width="213"height="378" bgcolor="#00FF99"><img src="http://192.168.1.169/proyecto_catalogo/recursos/imagenes/fotos/'+ producto.tenis[x].clave_zapato +'.jpg" width="213" height="343"></th><th width="354" bgcolor="#00FF99"><p>'+ producto.tenis[$x].modelo +' </p><p>'+ producto.tenis[$x].marca +'</p><p>'+ producto.tenis[$x].color_disponible +'</p><p>'+ producto.tenis[$x].precio +'</p><p>'+ producto.tenis[$x].descripcion +'</p><p>'+ producto.tenis[$x].talla_disponible +'</p></th></tr></table></div></div>');}
 			$(':mobile-pagecontainer').pagecontainer('change','#pagina1',{transition:'pop'});
 			
 			}
